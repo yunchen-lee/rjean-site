d88e5db5af70ebe2430266f21df026e66425c614
Yun-Chen Lee <yclee@arch.nycu.edu.tw>
2025-09-02 14:22:43 +0800

    update back to top btn

--- src/components/Back_to_top.jsx
+++ src/components/Back_to_top.jsx
@@ -19,10 +19,11 @@ export default function BackToTopButton() {
   return (
     <button
       onClick={scrollToTop}
-      class={`cursor-pointer fixed bottom-6 left-6 w-12 h-12 rounded-full bg-white text-white text-xl flex items-center justify-center shadow-lg transition-opacity duration-300 z-50 ${
+      class={`cursor-pointer fixed bottom-6 left-6 w-12 h-12 rounded-full bg-white text-white text-xl flex items-center justify-center shadow-lg transition-opacity duration-300 z-50  ${
         visible ? 'opacity-100' : 'opacity-0 pointer-events-none'
       }`}
-      aria-label="Back to Top"
+      aria-label="Back to Top" 
+      style={{ WebkitAppearance: 'none', appearance: 'none', backgroundColor: '#ffffffff' }}
     >
     <img src="/assets/arrow.svg" alt="Back to Top" class="w-6 h-6" />
     </button>
